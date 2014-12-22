90f42313017072d1db21d0d972e5825640d7203a
bysag <214994708@qq.com>
2014-12-22 15:45:54 +0800

    commit log

--- src/main/java/com/zuhu/system/controller/AbstractController.java
+++ src/main/java/com/zuhu/system/controller/AbstractController.java
@@ -8,6 +8,7 @@ public abstract class AbstractController {
     public Map<String, Object> generatorResult(Object res) {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("result", res);
+        System.out.println("Test");
         return result;
     }
 }
